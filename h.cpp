4943bac90f558ea508496e149098a37b9f7bcf73
chereshnya123 <nikos08012@gmail.com>
2024-03-11 10:19:56 +0300

    f22

--- h.cpp
+++ h.cpp
@@ -2,7 +2,7 @@
 
 int main() {
     std::cout << "feature 1" << std::endl;
-    std::cout << "feature 2" << std::endl;
+    std::cout << "feature 2.2" << std::endl;
     std::cout << "Hello world!" << std::endl;
 
     return 0;
